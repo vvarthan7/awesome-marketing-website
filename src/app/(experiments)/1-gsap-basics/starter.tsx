7a2cd856002ebd573ed90c4ff7b21cf5e22cd6ac
VISHNUVARTHAN U <vvarthan7@gmail.com>
2025-12-08 13:43:04 +0530

    Gsap-css-animation-updates

--- src/app/(experiments)/1-gsap-basics/starter.tsx
+++ src/app/(experiments)/1-gsap-basics/starter.tsx
@@ -1,9 +1,41 @@
 "use client";
 
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { useRef } from "react";
+import { SplitText } from "gsap/all";
+
+gsap.registerPlugin(SplitText);
+
 export default function Page() {
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  useGSAP(
+    () => {
+      const split = SplitText.create(".title", {
+        type: "chars, words",
+        charsClass: "letter",
+      });
+
+      gsap.from(".title .letter", {
+        autoAlpha: 0,
+        y: 200,
+        duration: 0.4,
+        stagger: 0.03,
+        ease: "circ.out",
+      });
+    },
+    {
+      scope: containerRef,
+    }
+  );
+
   return (
     <div className="bg-blue-300 text-black">
-      <div className="flex h-screen items-end justify-left overflow-hidden">
+      <div
+        ref={containerRef}
+        className="flex h-screen items-end justify-left overflow-hidden"
+      >
         <h1 className="title font-black text-[min(20rem,30vw)] leading-none pb-[0.1em] text-left">
           GSAP
           <br />
--- src/app/(experiments)/2-css-version/starter/index.tsx
+++ src/app/(experiments)/2-css-version/starter/index.tsx
@@ -1,11 +1,39 @@
+import s from "./styles.module.css";
+
+const text = ["CSS".split(""), "version".split("")];
+
 export default function Page() {
   return (
     <div className="bg-blue-300 text-black">
       <div className="flex h-screen items-end justify-left overflow-hidden">
         <h1 className="title font-black text-[min(20rem,30vw)] leading-none pb-[0.1em] text-left">
-          CSS
+          {text[0].map((letter, i) => (
+            <span
+              key={i}
+              className={s.letter}
+              style={
+                {
+                  "--index": i,
+                } as React.CSSProperties
+              }
+            >
+              {letter}
+            </span>
+          ))}
           <br />
-          version
+          {text[1].map((letter, i) => (
+            <span
+              key={i}
+              className={s.letter}
+              style={
+                {
+                  "--index": i,
+                } as React.CSSProperties
+              }
+            >
+              {letter}
+            </span>
+          ))}
         </h1>
       </div>
     </div>
