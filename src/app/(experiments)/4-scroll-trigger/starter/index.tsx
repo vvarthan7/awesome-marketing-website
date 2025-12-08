c8ffe34d161ffdffa68bb1a2cfb3ca6f89778ae0
VISHNUVARTHAN U <vvarthan7@gmail.com>
2025-12-08 23:08:46 +0530

    Scroll-trigger-update

--- src/app/(experiments)/4-scroll-trigger/starter/index.tsx
+++ src/app/(experiments)/4-scroll-trigger/starter/index.tsx
@@ -5,6 +5,10 @@ import { Highlighted1, Highlighted2, Highlighted3 } from "./highlights";
 import { TitleSection } from "./title";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
+import { ScrollTrigger, DrawSVGPlugin } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(DrawSVGPlugin);
 
 export default function Page() {
   return (
@@ -21,11 +25,24 @@ function DescriptionSection() {
 
   useGSAP(
     () => {
-      const tl = gsap.timeline();
+      const tl = gsap.timeline({
+        scrollTrigger: {
+          trigger: "h2",
+          start: "top center",
+          end: "bottom center",
+          toggleActions: "play reverse play reset",
+          markers: true,
+        },
+      });
 
       tl.from("h2", {
         opacity: 0,
         duration: 1,
+      });
+
+      tl.from("path", {
+        drawSVG: 0,
+        stagger: 0.1,
       });
     },
     {
