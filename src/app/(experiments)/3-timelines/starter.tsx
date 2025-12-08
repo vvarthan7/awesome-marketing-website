9b982d10fd4655a407d1f425203b4842c0487f96
VISHNUVARTHAN U <vvarthan7@gmail.com>
2025-12-08 19:27:22 +0530

    timeline-animation-update

--- src/app/(experiments)/3-timelines/starter.tsx
+++ src/app/(experiments)/3-timelines/starter.tsx
@@ -3,14 +3,84 @@
 import { useGSAP } from "@gsap/react";
 import { useRef } from "react";
 
-import { SplitText } from "gsap/all";
+import { SplitText, GSDevTools } from "gsap/all";
 import gsap from "gsap";
 gsap.registerPlugin(SplitText);
+gsap.registerPlugin(GSDevTools);
 
 export default function Page() {
   const containerRef = useRef<HTMLDivElement>(null);
 
-  useGSAP(() => {}, { scope: containerRef });
+  useGSAP(
+    () => {
+      SplitText.create(".title", {
+        type: "words, chars",
+        wordsClass: "word++",
+        charsClass: "char",
+        mask: "chars",
+      });
+      const t1 = gsap.timeline();
+      GSDevTools.create({ animation: t1 });
+
+      t1.from(".title .word1 .char", {
+        y: "100%",
+        duration: 0.5,
+        stagger: 0.07,
+        ease: "circ.out",
+      });
+      t1.from(
+        ".title .word2 .char",
+        {
+          x: "-100%",
+          duration: 0.2,
+          stagger: 0.07,
+          ease: "circ.out",
+        },
+        "-=0.5s"
+      );
+      t1.from(
+        ".tl-dot",
+        {
+          opacity: 0,
+          duration: 0.01,
+          repeat: 6,
+          yoyo: true,
+          repeatDelay: 0.05,
+          ease: "circ.out",
+        },
+        "<"
+      );
+      t1.from(
+        ".tl-start",
+        {
+          height: 0,
+          duration: 0.4,
+          ease: "circ.out",
+        },
+        "<"
+      );
+      t1.from(
+        ".tl-main",
+        {
+          width: 0,
+          duration: 0.8,
+          ease: "circ.out",
+        },
+        "<+0.2s"
+      );
+      t1.from(
+        ".title .word3 .char",
+        {
+          y: "-100%",
+          duration: 0.3,
+          stagger: 0.07,
+          ease: "circ.out",
+        },
+        "-=0.5s"
+      );
+    },
+    { scope: containerRef }
+  );
 
   return (
     <div
