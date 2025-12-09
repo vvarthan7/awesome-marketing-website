509e0f59ec9939e4484c141c39436d27178b404e
VISHNUVARTHAN U <vvarthan7@gmail.com>
2025-12-09 18:21:55 +0530

    font-animation-update

--- src/app/(experiments)/6-variables/starter/index.tsx
+++ src/app/(experiments)/6-variables/starter/index.tsx
@@ -2,8 +2,40 @@
 
 import { cn } from "@/lib/utils";
 import s from "./styles.module.css";
+import { useEffect, useRef, useState } from "react";
+import { distance } from "@/lib/math";
 
 export default function Page() {
+  const titleRef = useRef<HTMLHeadingElement>(null);
+
+  useEffect(() => {
+    const controller = new AbortController();
+
+    window.addEventListener(
+      "mousemove",
+      (event) => {
+        const { clientX, clientY } = event;
+        const screenW = window.innerWidth;
+        const screenH = window.innerHeight;
+        const centerX = screenW / 2;
+        const centerY = screenH / 2;
+        const d = distance(clientX, clientY, centerX, centerY);
+        const maxDistance = distance(0, 0, centerX, centerY);
+        titleRef.current?.style.setProperty(
+          "--distance",
+          (d / maxDistance).toString()
+        );
+      },
+      {
+        signal: controller.signal,
+      }
+    );
+
+    return () => {
+      controller.abort();
+    };
+  }, []);
+
   return (
     <div
       className={cn(
@@ -11,7 +43,12 @@ export default function Page() {
         s.grid
       )}
     >
+      <script
+        crossOrigin="anonymous"
+        src="//unpkg.com/react-scan/dist/auto.global.js"
+      />
       <h1
+        ref={titleRef}
         className={cn(
           "uppercase text-[10vh] leading-none relative",
           s["title"]
