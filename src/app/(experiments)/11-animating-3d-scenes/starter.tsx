288c35c641bd4e64faefd42503c30b424e8b152e
VISHNUVARTHAN U <vvarthan7@gmail.com>
2025-12-10 18:10:36 +0530

    animating-3d-update

--- src/app/(experiments)/11-animating-3d-scenes/starter.tsx
+++ src/app/(experiments)/11-animating-3d-scenes/starter.tsx
@@ -1,19 +1,54 @@
 "use client";
 
+import { useGSAP } from "@gsap/react";
 import { PerspectiveCamera, useGLTF } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
+import gsap from "gsap";
+import { useState } from "react";
+import { Group, PointLight } from "three";
 
 export default function Page() {
+  const [groupRef, setGroupRef] = useState<Group | null>(null);
+  const [lightRef, setLightRef] = useState<PointLight | null>(null);
+
+  useGSAP(
+    () => {
+      if (!groupRef || !lightRef) return;
+      gsap.from(groupRef.position, {
+        y: -1,
+        duration: 1,
+        ease: "power2.out",
+      });
+
+      gsap.from(groupRef.rotation, {
+        x: 0.5,
+        duration: 1,
+        ease: "power2.out",
+      });
+
+      gsap.from(lightRef, {
+        intensity: 0,
+        duration: 1.2,
+        ease: "power2.out",
+      });
+
+      gsap.to(lightRef.position, {
+        x: -4,
+      });
+    },
+    {
+      dependencies: [groupRef, lightRef],
+    }
+  );
+
   return (
     <div className="absolute h-screen w-screen top-0 left-0 bg-black">
       <Canvas>
-        <group>
+        <group ref={setGroupRef}>
           <Scene />
         </group>
+        <pointLight ref={setLightRef} position={[10, 2, 2]} intensity={10} />
         <PerspectiveCamera fov={28} makeDefault position={[0, 1.5, 5]} />
-        <ambientLight intensity={0.1} />
-        <pointLight position={[30, 3, 5]} intensity={20} />
-        <color args={["black"]} attach="background" />
       </Canvas>
     </div>
   );
