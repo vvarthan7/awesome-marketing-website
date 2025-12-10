9fbc78221cfc7a050f46ac79bd005637bda5fb65
VISHNUVARTHAN U <vvarthan7@gmail.com>
2025-12-10 17:33:28 +0530

    device-profiling-updates

--- src/app/(experiments)/10-device-profiling/starter/index.tsx
+++ src/app/(experiments)/10-device-profiling/starter/index.tsx
@@ -1,7 +1,52 @@
 "use client";
 
+import { useEffect, useState } from "react";
+import { StaticVersion } from "./logo-static";
 import { ShaderEffect } from "./logo-webgl";
+import { getGPUTier } from "detect-gpu";
+import { getSelectorsByUserAgent } from "react-device-detect";
+import { DeviceType } from "@/types";
+import { useBattery } from "@/hooks/use-battery";
 
 export default function Page() {
-  return <ShaderEffect />;
+  const shouldUseGl = useShouldRenderGl();
+  return shouldUseGl ? <ShaderEffect /> : <StaticVersion />;
+}
+
+function useShouldRenderGl() {
+  const [gpuTier, setGpuTier] = useState<number | null>(null);
+  const [deviceInfo, setDeviceInfo] = useState<DeviceType | null>(null);
+  const battery = useBattery();
+
+  useEffect(() => {
+    const result: DeviceType = getSelectorsByUserAgent(
+      window.navigator.userAgent
+    );
+
+    // eslint-disable-next-line react-hooks/set-state-in-effect
+    setDeviceInfo(result);
+
+    getGPUTier().then((result) => {
+      setGpuTier(result.tier);
+    });
+  }, []);
+
+  if (typeof gpuTier == "number" && gpuTier < 2) {
+    return false;
+  }
+
+  if (deviceInfo && deviceInfo.isSafari) {
+    return false;
+  }
+
+  if (
+    battery.isSupported &&
+    battery.fetched &&
+    battery.level < 0.3 &&
+    !battery.charging
+  ) {
+    return false;
+  }
+
+  return true;
 }
